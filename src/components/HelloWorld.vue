f4c76aea90de70544331bc4c5586f1c2ec4eef97
florin-ciornei <31960645+florin-ciornei@users.noreply.github.com>
2020-08-15 17:02:59 +0200

    Setup Vue project

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <button type="button" @click="textEncrypt()">Test encrypt</button>
+    {{encryptResult}}
+    <br />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+import * as Encryption from "./../logic/Encryption";
+
+@Component
+export default class HelloWorld extends Vue {
+  @Prop() private msg!: string;
+
+  encryptResult = "nothing";
+
+  textEncrypt() {
+    this.encryptResult = Encryption.testMethod();
+  }
+}
+</script>
+
+<style scoped lang="scss">
+</style>
---
+++ src/logic/Encryption.ts
@@ -0,0 +1,20 @@
+export function testMethod(): string {
+    return "it works";
+}
+
+//some more typescript examples. This stuff can be also in separate files, it will need be imported.
+class DemoClass {
+    name(params: number): string {
+        return "asd";
+    }
+}
+
+interface DemoInterface {
+    a(): number
+}
+
+class DemoClassInterface implements DemoInterface {
+    a(): number {
+        throw new Error("Method not implemented.");
+    }
+}
