9c32079b58934a2e8ee6059f7e7f697db2295398
florin-ciornei <31960645+florin-ciornei@users.noreply.github.com>
2020-08-17 17:42:22 +0200

    Add internationalization

--- vue.config.js
+++ vue.config.js
@@ -5,5 +5,14 @@ module.exports = {
         prependData: `@import "@/assets/css/_materialColorsVariables.scss";@import "@/assets/css/_variables.scss";`
       }
     }
+  },
+
+  pluginOptions: {
+    i18n: {
+      locale: "en",
+      fallbackLocale: "en",
+      localeDir: "locales",
+      enableInSFC: false
+    }
   }
 };
